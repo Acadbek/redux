d4c60724131ac4c3111ef9939683bbad84ace356
Asadbek <asadbeknan5566@gmail.com>
2023-01-09 20:09:03 +0500

    create redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from './App'
+import {createStore} from "redux";
 
+
+// ACTIONS
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+// REDUCER
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+    }
+}
+
+let store = createStore(counter)
+
+// Display it in the console
+store.subscribe(() => console.log(store.getState()))
+
+// DISPATCH
+
+store.dispatch(increment())
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
